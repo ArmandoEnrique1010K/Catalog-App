e7f45297832a795c262c162fddf6206a94858e25
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-01-01 23:26:42 -0500

    creación del backend

---
+++ backend-catalogapp/src/main/java/com/backend/catalogapp/BackendCatalogappApplication.java
@@ -0,0 +1,13 @@
+package com.backend.catalogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCatalogappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCatalogappApplication.class, args);
+	}
+
+}

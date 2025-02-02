437fc09da0dc5276392c9bbef061c7a51e2ac04a
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-02-02 12:31:53 -0500

    El tipo String no sirve para las validaciones

--- backend-catalogapp/src/main/java/com/backend/catalogapp/controllers/ProductController.java
+++ backend-catalogapp/src/main/java/com/backend/catalogapp/controllers/ProductController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +39,9 @@ import com.backend.catalogapp.services.ProductService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Valid;
+import jakarta.validation.Validation;
 
 @RestController
 @RequestMapping("/products")
@@ -73,69 +77,68 @@ public class ProductController {
         return productService.findById(id).orElseThrow();
     }
 
-    // // TODO: USAR UN ModelAttribute Y RequestParam por separado, uno para el JSON
-    // @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    // public ResponseEntity<?> create(
-    // @RequestParam("name") String name,
-    // @RequestParam("code") String code,
-    // @RequestParam("price") Double price,
-    // @RequestParam("inOffer") Boolean inOffer,
-    // @RequestParam(value = "offerPrice", required = false) Double offerPrice,
-    // @RequestParam("description") String description,
-    // @RequestParam("brandId") Long brandId,
-    // @RequestParam("categoryId") Long categoryId,
-    // @RequestParam("image") MultipartFile image) {
+    // TODO: USAR UN ModelAttribute Y RequestParam por separado, uno para el JSON
+    @PostMapping(value = "/test", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> createTest(
+            @RequestParam("name") String name,
+            @RequestParam("code") String code,
+            @RequestParam("price") Double price,
+            @RequestParam("inOffer") Boolean inOffer,
+            @RequestParam(value = "offerPrice", required = false) Double offerPrice,
+            @RequestParam("description") String description,
+            @RequestParam("brandId") Long brandId,
+            @RequestParam("categoryId") Long categoryId,
+            @RequestParam("image") MultipartFile image) {
 
-    // // logger.info("Inicio de creación de producto");
+        // logger.info("Inicio de creación de producto");
 
-    // // logger.debug("Datos recibidos: name={}, code={}, price={}, inOffer={},
-    // // offerPrice={}, description={}, brandId={}, categoryId={}, image={}",
-    // // name, code, price, inOffer, offerPrice, description, brandId, categoryId,
-    // // image.getOriginalFilename());
+        // logger.debug("Datos recibidos: name={}, code={}, price={}, inOffer={},
+        // offerPrice={}, description={}, brandId={}, categoryId={}, image={}",
+        // name, code, price, inOffer, offerPrice, description, brandId, categoryId,
+        // image.getOriginalFilename());
 
-    // if (image.isEmpty()) {
-    // // logger.warn("Imagen vacía recibida");
-    // return ResponseEntity.badRequest().body("Debe proporcionar una imagen
-    // válida.");
-    // }
+        if (image.isEmpty()) {
+            // logger.warn("Imagen vacía recibida");
+            return ResponseEntity.badRequest().body("Debe proporcionar una imagen válida.");
+        }
 
-    // // Crear el objeto Product con los datos recibidos
-    // Product product = new Product();
-    // product.setName(name);
-    // product.setCode(code);
-    // product.setPrice(price);
-    // product.setInOffer(inOffer);
-    // product.setOfferPrice(inOffer ? offerPrice : null);
-    // product.setDescription(description);
-    // // logger.info("Producto creado en memoria sin relaciones");
+        // Crear el objeto Product con los datos recibidos
+        Product product = new Product();
+        product.setName(name);
+        product.setCode(code);
+        product.setPrice(price);
+        product.setInOffer(inOffer);
+        product.setOfferPrice(inOffer ? offerPrice : null);
+        product.setDescription(description);
+        // logger.info("Producto creado en memoria sin relaciones");
 
-    // // Asignar la marca y la categoría
-    // product.setBrand(new Brand());
-    // product.getBrand().setId(brandId);
-    // product.setCategory(new Category());
-    // product.getCategory().setId(categoryId);
+        // Asignar la marca y la categoría
+        product.setBrand(new Brand());
+        product.getBrand().setId(brandId);
+        product.setCategory(new Category());
+        product.getCategory().setId(categoryId);
 
-    // // logger.info("Marca y categoría asignadas");
+        // logger.info("Marca y categoría asignadas");
 
-    // // Asignar la imagen recibida
-    // product.setImage(new Image());
-    // product.getImage().setFile(image);
+        // Asignar la imagen recibida
+        product.setImage(new Image());
+        product.getImage().setFile(image);
 
-    // // logger.info("Imagen asignada al producto: {}",
-    // image.getOriginalFilename());
+        // logger.info("Imagen asignada al producto: {}",
+        image.getOriginalFilename();
 
-    // try {
-    // ProductDetailDto savedProduct = productService.save(product, image);
-    // // logger.info("Producto guardado correctamente con ID: {}",
-    // // savedProduct.getId());
-    // return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
-    // } catch (Exception e) {
-    // // logger.error("Error al guardar el producto: {}", e.getMessage(), e);
-    // return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-    // .body("Error al guardar el producto: " + e.getMessage());
-    // }
+        try {
+            ProductDetailDto savedProduct = productService.save(product, image);
+            // logger.info("Producto guardado correctamente con ID: {}",
+            // savedProduct.getId());
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+        } catch (Exception e) {
+            // logger.error("Error al guardar el producto: {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Error al guardar el producto: " + e.getMessage());
+        }
 
-    // }
+    }
 
     // TODO: USAR UN ModelAttribute Y RequestParam por separado, uno para el JSON
     // REPARAR ESTE ENDPOINT
@@ -151,6 +154,7 @@ public class ProductController {
         // Convertir JSON a objeto Product
         ObjectMapper objectMapper = new ObjectMapper();
         Product product;
+        // TODO: INVESTIGAR COMO MEJORAR Y OBTENER EL MENSAJE DE ERROR
         try {
             product = objectMapper.readValue(productJson, Product.class);
         } catch (JsonProcessingException e) {
@@ -158,6 +162,7 @@ public class ProductController {
         }
 
         if (result.hasErrors()) {
+            System.out.println("Errores de validación: " + validation(result).getBody());
             return validation(result);
         }
 
@@ -171,13 +176,15 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Error al guardar el producto: " + e.getMessage());
+                    .body(validation(result));
+            // return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            // .body("Error al guardar el producto: " + e.getMessage());
         }
 
     }
 
-    @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> update(
+    @PutMapping(value = "/test/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateTest(
 
             @RequestParam("name") String name,
             @RequestParam("code") String code,
@@ -230,15 +237,52 @@ public class ProductController {
 
     }
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<?> remove(@PathVariable Long id) {
-        Optional<ProductDetailDto> o = productService.findById(id);
+    @PutMapping(value = "/{id}", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE })
+    public ResponseEntity<?> update(
+            @RequestPart("product") String productJson, BindingResult result,
+            @RequestPart(value = "image", required = false) MultipartFile image,
+            @PathVariable Long id) {
 
-        if (o.isPresent()) {
-            productService.remove(id);
-            return ResponseEntity.noContent().build(); // 204
+        // Convertir JSON a objeto Product
+        ObjectMapper objectMapper = new ObjectMapper();
+        Product product;
+        try {
+            product = objectMapper.readValue(productJson, Product.class);
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.badRequest().body("Error al procesar JSON: " + e.getMessage());
         }
-        return ResponseEntity.notFound().build(); // 404
+
+        // VALIDACIÓN MANUAL de campos
+
+        // Set<ConstraintViolation<Product>> violations =
+        // Validation.buildDefaultValidatorFactory()
+        // .getValidator().validate(product);
+
+        // if (!violations.isEmpty()) {
+        // List<String> errorMessages = violations.stream()
+        // .map(violation -> violation.getPropertyPath() + " " + violation.getMessage())
+        // .collect(Collectors.toList());
+        // return ResponseEntity.badRequest().body(errorMessages);
+        // }
+
+        if (result.hasErrors()) {
+            System.out.println("Errores de validación: " + validation(result).getBody());
+            return validation(result);
+        }
+
+        if (image != null && !image.isEmpty()) {
+            product.setImage(new Image());
+            product.getImage().setFile(image);
+        }
+
+        try {
+            Optional<ProductDetailDto> o = productService.update(product, image, id);
+            return o.map(ResponseEntity::ok)
+                    .orElseGet(() -> ResponseEntity.notFound().build());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al actualizar el producto: " + e.getMessage());
+        }
     }
 
     private ResponseEntity<?> validation(BindingResult result) {
@@ -248,6 +292,17 @@ public class ProductController {
             errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> remove(@PathVariable Long id) {
+        Optional<ProductDetailDto> o = productService.findById(id);
+
+        if (o.isPresent()) {
+            productService.remove(id);
+            return ResponseEntity.noContent().build(); // 204
+        }
+        return ResponseEntity.notFound().build(); // 404
     }
 
     // TODO: Tener en cuenta los siguientes endpoints
